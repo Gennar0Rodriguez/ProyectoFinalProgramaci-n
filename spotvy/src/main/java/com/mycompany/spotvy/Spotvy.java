00de036b67e9f8e6f564cf214d594a8772ffa307
Gennaro Rodriguez <brawllow@gmail.com>
2025-09-25 05:00:54 -0300

    onichaaaan push push push push push

--- spotvy/src/main/java/com/mycompany/spotvy/Spotvy.java
+++ spotvy/src/main/java/com/mycompany/spotvy/Spotvy.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.spotvy;
 
 import logica.Usuario;
@@ -9,6 +8,8 @@ import logica.Cantante;
 import logica.Cancion;
 import logica.Banda;
 import Manejadores.ManejadorCanciones;
+import java.util.Scanner;
+import Manejadores.ManejadorUsuario;
 
 /**
  *
@@ -17,16 +18,38 @@ import Manejadores.ManejadorCanciones;
 public class Spotvy {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        System.out.println("--------------");
         
         // DEMO    
-        Cancion can = new Cancion("SeFueTv");   
-        ManejadorCanciones mc = ManejadorCanciones.getInstance();   
-        mc.agregarCancion(can);   
-        mc.printCanciones();        
+        Cancion can = new Cancion("SeFueTv");
+        ManejadorCanciones mc = ManejadorCanciones.getInstance();
+        mc.agregarCancion(can);
+        mc.printCanciones();
         //
         
-       
+        System.out.println("--------------");
         
+        // DEMO CON SCANNER
+        Scanner cancion = new Scanner(System.in);
+        System.out.println("Diga su canción");
+        String nc = cancion.nextLine();
+        Cancion can2 = new Cancion(nc);
+        mc.agregarCancion(can2);
+        mc.printCanciones();
+        
+        System.out.println("--------------");
+        
+        System.out.println("Usuarios actualmente:");
+        Cantante system = new Cantante("system", "nahuel", "123", 0, 0);
+        ManejadorUsuario nu = ManejadorUsuario.getInstance();
+        nu.agregarUsuario(system);
+        nu.printUsuarios();
+        
+        Usuario usuario = nu.getUsuario("system");
+        if(usuario instanceof Cantante){
+            Cantante c = (Cantante) usuario;
+        c.agregarCancion(can2);
+        }
     }
 }
